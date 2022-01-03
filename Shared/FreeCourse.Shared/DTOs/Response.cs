5782ebed89f9c2667278ef373dc3f10f2db3bf74
Yasin Mataracı <yasinmataraci@gmail.com>
2022-01-03 23:15:56 +0300

    dtolar eklendi. automapper kullanıldı. kategori servisi yapıldı

---
+++ Shared/FreeCourse.Shared/DTOs/Response.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.DTOs
+{
+    public class Response<T>
+    {
+        public T Data { get; private set; }
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccess { get; private set; }
+        public List<string> Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccess = true };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccess = true };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T> { StatusCode = statusCode, IsSuccess = false, Errors = errors };
+        }
+
+        public static Response<T> Fail(string error, int statusCode)
+        {
+            return new Response<T> { StatusCode = statusCode, IsSuccess = false, Errors = new List<string>() { error } };
+        }
+    }
+}
