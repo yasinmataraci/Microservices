300784147dc05d8fd7944afcdb16e0038f99ac02
Yasin Mataracı <yasinmataraci@gmail.com>
2022-01-05 01:30:35 +0300

    identity server eklendi

---
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace FreeCourse.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermision"}},
+            new ApiResource("resource_photostock"){Scopes={"photostock_fullpermision"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("catalog_fullpermision","Catalog API için full erişim"),
+                new ApiScope("photostock_fullpermision","PhotoStock API için full erişim"),
+                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+               new Client{
+                   ClientName="Asp.Net Core MVC",
+                   ClientId="WebMVCClient",
+                   ClientSecrets={new Secret("secret".Sha256())},
+                   AllowedGrantTypes=GrantTypes.ClientCredentials,
+                   AllowedScopes={ "catalog_fullpermision", "photostock_fullpermision",IdentityServerConstants.LocalApi.ScopeName}
+               }
+            };
+    }
+}
---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using FreeCourse.IdentityServer.DTOs;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDTO signUpDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDTO.UserName,
+                Email = signUpDTO.Email,
+                City = signUpDTO.City
+            };
+            var result = await _userManager.CreateAsync(user, signUpDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(), 404));
+            }
+
+            return NoContent();
+        }
+    }
+}
