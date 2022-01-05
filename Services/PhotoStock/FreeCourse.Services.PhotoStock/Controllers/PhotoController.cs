17a71251ece9a206f4606a7199dab23aec385e65
Yasin Mataracı <yasinmataraci@gmail.com>
2022-01-05 14:19:03 +0300

    photo ekleme işlemi

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,58 @@
+﻿using FreeCourse.Services.PhotoStock.DTOs;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDTO photoDTO = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDTO>.Success(photoDTO, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDTO>.Fail("photo is empty", 400));
+        }
+
+        [HttpGet]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/DTOs/PhotoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.DTOs
+{
+    public class PhotoDTO
+    {
+        public string Url { get; set; }
+    }
+}
